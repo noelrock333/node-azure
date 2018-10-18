f9ad9ecd37a25da2d55ba307a5896e94275da43b
Noel Escobedo <noel@michelada.io>
2018-10-18 00:48:40 +0000

    Added axios for ajax calls

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var express = require('express');
 var app = express();
 var azure = require('azure');
 var port = process.env.PORT || 3000;
+var axios = require('axios');
 
 var serviceBusService = azure.createServiceBusService();
 serviceBusService.createQueueIfNotExists('myqueue', function(error){
@@ -48,6 +49,61 @@ app.get('/create_topic_message', function(req, res, next) {
     }
   });
 });
+
+app.get('/ocapi', function(req, res, next) {
+  axios({
+    method: 'post',
+    url: 'https://dev03-store-modcloth.demandware.net/dw/oauth2/access_token?client_id=c2bbf8ca-ba84-470c-a87b-46115a37bb97',
+    data: 'grant_type=urn:demandware:params:oauth:grant-type:client-id:dwsid:dwsecuretoken',
+    headers: {
+        'Authorization': 'Basic bm9lbEBtaWNoZWxhZGEuaW86TWljaGVsYWRhXzY6VjgjM3IjUiVIQXZQVlAqJUhmNw==',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+  .then(function(response) {
+      if (response.data) {
+        axios({
+            method: 'get',
+            url: 'https://dev03-store-modcloth.demandware.net/s/modcloth/dw/shop/v16_9/orders/00016306',
+            headers: {
+                'Authorization': 'Bearer ' + response.data.access_token,
+                'Origin': 'https://dev03-store-modcloth.demandware.net'
+            },
+        }).then(function(order) {
+          if (order) {
+            axios({
+              method: 'patch',
+              url: 'https://dev03-store-modcloth.demandware.net/s/modcloth/dw/shop/v16_9/orders/00016306',
+              headers: {
+                  'Authorization': 'Bearer ' + response.data.access_token,
+                  'Origin': 'https://dev03-store-modcloth.demandware.net',
+                  'Content-Type': 'application/json'
+              },
+              data: {
+                customer_name: 'Jhony'
+              }
+            }).then(function(modOrder) {
+              // console.log(modOrder.data)
+              res.json(modOrder.data);
+            }).catch(function(err) {
+              console.log(err)
+              res.send('');
+            })
+          } else {
+            res.json({ message: 'Error' });
+          }
+          // res.json(order.data);
+        }).catch(function(err) {
+            console.log(err);
+            res.json(err);
+        })
+      }
+  })
+  .catch(function(err) {
+      res.json(err);
+      console.log(err);
+  });
+})
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
